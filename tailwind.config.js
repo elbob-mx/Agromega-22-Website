ccee670d11de9e529d527531fc23c00532da6838
elbob-mx <rob7th@gmail.com>
2022-09-20 15:17:03 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      small: '0px',
+      mobile: '359px',
+      mobileHor: '640px',
+      tablet: '768px',
+      laptop: '976px',
+      desktop: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        'rubik': 'Rubik, sans-serif',
+        },
+    },
   },
   plugins: [],
 }
