13403e6795707be8eb87a3d143c452a286ad8cf3
elbob-mx <rob7th@gmail.com>
2022-11-02 12:45:59 -0600

    leer más button

---
+++ js/toggle-button.js
@@ -0,0 +1,25 @@
+function toggleText() {
+  var puntos = document.getElementById("puntos");
+  var masTexto = document.getElementById("mas");
+  var leerMas = document.getElementById("leer-mas");
+
+  if (puntos.classList.contains("hidden")) {
+      // Mostrar puntos suspensivos
+      puntos.classList.remove("hidden");
+
+      // Esconder más texto
+      masTexto.classList.add("hidden");
+
+      // cambiar texto botón
+      leerMas.innerHTML = "leer más";
+  } else {
+      // Esconder los puntos
+      puntos.classList.add("hidden");
+
+      // Esconder más texto
+      masTexto.classList.remove("hidden");
+
+      // cambiar texto botón
+      leerMas.innerHTML = "leer menos";
+  }
+}
