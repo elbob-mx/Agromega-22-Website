b97a2f604ce0454869d4cd0062238e4b857f1be2
elbob-mx <rob7th@gmail.com>
2022-11-03 10:19:17 -0600

    images alt update

--- js/toggle-button.js
+++ js/toggle-button.js
@@ -1,25 +1,21 @@
 // FUNCTIONS --------->
 function switchTexto() {
-  var puntos = document.getElementById("puntos");
-  var masTexto = document.getElementById("mas");
-  var leerMas = document.getElementById("leer-mas");
+  let puntos = document.getElementById("puntos");
+  let masTexto = document.getElementById("mas");
+  let leerMas = document.getElementById("leer-mas");
 
   if (puntos.classList.contains("hidden")) {
       // Mostrar puntos suspensivos
       puntos.classList.remove("hidden");
-
       // Esconder más texto
       masTexto.classList.add("hidden");
-
       // cambiar texto botón
       leerMas.innerHTML = "leer más";
   } else {
       // Esconder los puntos
       puntos.classList.add("hidden");
-
       // Esconder más texto
       masTexto.classList.remove("hidden");
-
       // cambiar texto botón
       leerMas.innerHTML = "leer menos";
   }
