fac19f347d6d52129a89f2fda0be31e3ee6d338b
elbob-mx <rob7th@gmail.com>
2022-11-18 14:21:31 -0600

    historia function update

--- js/toggle-button.js
+++ js/toggle-button.js
@@ -1,22 +1,28 @@
 // FUNCTIONS --------->
-function switchTexto() {
-  let puntos = document.getElementById("puntos");
-  let masTexto = document.getElementById("mas");
-  let leerMas = document.getElementById("leer-mas");
+// function switchTexto() {
+//   let puntos = document.getElementById("puntos");
+//   let masTexto = document.getElementById("mas");
+//   let leerMas = document.getElementById("leer-mas");
 
-  if (puntos.classList.contains("hidden")) {
-      // Mostrar puntos suspensivos
-      puntos.classList.remove("hidden");
-      // Esconder más texto
-      masTexto.classList.add("hidden");
-      // cambiar texto botón
-      leerMas.innerHTML = "leer más";
-  } else {
-      // Esconder los puntos
-      puntos.classList.add("hidden");
-      // Esconder más texto
-      masTexto.classList.remove("hidden");
-      // cambiar texto botón
-      leerMas.innerHTML = "leer menos";
-  }
-}
+//   if (puntos.classList.contains("hidden")) {
+//       // Mostrar puntos suspensivos
+//       puntos.classList.remove("hidden");
+//       // Esconder más texto
+//       masTexto.classList.add("hidden");
+//       // cambiar texto botón
+//       leerMas.innerHTML = "leer más";
+//   } else {
+//       // Esconder los puntos
+//       puntos.classList.add("hidden");
+//       // Esconder más texto
+//       masTexto.classList.remove("hidden");
+//       // cambiar texto botón
+//       leerMas.innerHTML = "leer menos";
+//   }
+// }
+
+function switchTexto() {
+const historia = "Inició hace 18 años, como una pequeña comercializadora que vendía agroquímicos y semillas. Hoy en día seguimos vendiendo agroquímicos y semillas, pero también fertilizantes, equipos de aplicación, equipos de riego y más. Ese “más” es el valor agregado que brindamos a través del mejor servicio al cliente, con soluciones integrales y una cultura de trabajo que nos permita generar relaciones de confianza a largo plazo."
+
+document.getElementById("mas").innerHTML = historia;
+};
