bc2b169ba9f25eb15a911b97f4625351fdd9e790
elbob-mx <rob7th@gmail.com>
2023-11-15 13:38:50 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     screens: {
-      small: '0px',
-      mobile: '359px',
-      mobileHor: '640px',
-      tablet: '768px',
-      laptop: '976px',
-      ipad: '1023px',
-      hd:'1365px',
-      fhd: '1440px',
+      small: "0px",
+      mobile: "359px",
+      mobileHor: "640px",
+      tablet: "768px",
+      laptop: "976px",
+      ipad: "1023px",
+      hd: "1365px",
+      fhd: "1440px",
     },
     extend: {
       fontFamily: {
-        'rubik': 'Rubik, sans-serif',
-        'plex': 'IBM Plex Sans Condensed, sans-serif',
-        },
-        colors: {
-          azulAgro: '#1755A1',
-          azulAgroClaro: '#00ABEA',
-          offWhite: '#F2F2F2',
-        },
+        rubik: "Rubik, sans-serif",
+        plex: "IBM Plex Sans Condensed, sans-serif",
+      },
+      colors: {
+        azulAgro: "#1755A1",
+        azulAgroClaro: "#00ABEA",
+        offWhite: "#F2F2F2",
+        offBlack: "#181818",
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
